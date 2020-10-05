6348f31e918ccf045345d6f6bdef4911b6461d77
Jacob <jacobm3@gmail.com>
2020-10-05 18:52:37 -0500

    Update gen-csr.sh

--- tls/gen-csr.sh
+++ tls/gen-csr.sh
@@ -1,39 +1,30 @@
 #!/bin/bash -x
 
-if [ -z "$1" ]
-  then
-    echo "usage: $0 NODENAME IPADDR"
-    exit 1
-fi
-
-if [ -z "$2" ]
-  then
-    echo "usage: $0 NODENAME IPADDR"
-    exit 1
-fi
-
-NODENAME=$1
-IPADDR=$2
-
-openssl req -out ${NODENAME}.req -newkey rsa:2048 -nodes -keyout ${NODENAME}.key -config - << EOF
+cat >csr.cnf <<EOF
 [req]
 distinguished_name = req_distinguished_name
-x509_extensions = v3_req
+req_extensions = v3_req
 prompt = no
 [req_distinguished_name]
 C = US
-ST = Texas
+ST = TX
 L = Houston
-O = Hashicorp
-OU = Engineering
-CN = ${NODENAME}.test.io
+O = Hashicorp POC
+OU = Solution Engineering
+CN = vault
 [v3_req]
+keyUsage = keyEncipherment, dataEncipherment
 extendedKeyUsage = serverAuth
 subjectAltName = @alt_names
 [alt_names]
-DNS.1 = ${NODENAME}
-DNS.2 = ${NODENAME}.test
-DNS.2 = ${NODENAME}.test.io
+DNS.1 = vault1.test.io
+DNS.2 = vault2.test.io
+DNS.3 = vault3.test.io
+DNS.4 = localhost
 IP.1 = 127.0.0.1
-IP.2 = ${IPADDR}
 EOF
+
+openssl req -out vault.req -newkey rsa:2048 -nodes -keyout vault.key -config csr.cnf
+
+echo "Request Details:"
+openssl req -noout -text -in vault.req
