ac9275fb02f5e6b930afae9c083c6c40076ab1ac
Jacob <jacobm3@gmail.com>
2020-10-05 23:25:50 -0500

    update

--- gen-vault-certs.sh
+++ gen-vault-certs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+DOMAIN=test.io
+
 cat >csr.cnf <<EOF
 [req]
 distinguished_name = req_distinguished_name
@@ -17,9 +19,9 @@ keyUsage = keyEncipherment, dataEncipherment
 extendedKeyUsage = serverAuth
 subjectAltName = @alt_names
 [alt_names]
-DNS.1 = vault1.test.io
-DNS.2 = vault2.test.io
-DNS.3 = vault3.test.io
+DNS.1 = vault1.${DOMAIN}
+DNS.2 = vault2.${DOMAIN}
+DNS.3 = vault3.${DOMAIN}
 DNS.4 = localhost
 IP.1 = 127.0.0.1
 EOF
@@ -29,5 +31,16 @@ openssl req -out vault.req -newkey rsa:2048 -nodes -keyout vault.key -config csr
 echo "Request Details:"
 openssl req -noout -text -in vault.req
 
-# Bug in easyrsa requires you to specify SANs on CLI, won't honor from CSR
-# easyrsa --subject-alt-name=DNS.1:vault1.test.io,DNS.2:vault2.test.io,DNS.3:vault3.test.io sign-req server vault
+
+./easyrsa import-req vault.req vault
+
+./easyrsa --subject-alt-name=DNS.1:vault1.test.io,DNS.2:vault2.test.io,DNS.3:vault3.test.io sign-req server vault
+
+openssl x509 -in /home/jacob/CA/pki/issued/vault.crt -text
+
+cp pki/ca.crt .
+cp pki/issued/vault.crt .
+tar zcf certs.tgz ca.crt vault.crt vault.key
+
+echo 
+echo "Certificates and key: certs.tgz"
